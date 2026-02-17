3cc47064d393c1d9b89029aa131819f2c44f1fb1
Anotherplay <novianrifqitri@gmail.com>
2026-02-17 12:36:24 +0700

    Update and rename login_database_pengisi.php to login_database_pengisi.json
    ubah tipe file

--- login_database_pengisi.php
+++
@@ -1,8 +0,0 @@
-$array = array(
-    array("val0"=>"Nama", "val1"=>"No. ID", "val2"=>"Tanggal Lahir"),
-    array("val0"=>"Icha", "val1"=>"0001", "val2"=>"17/04/2009"),
-    array("val0"=>"Abrar", "val1"=>"0002", "val2"=>"13/12/2008"),
-    array("val0"=>"Aqsal", "val1"=>"0003", "val2"=>"16/05/2008"),
-    array("val0"=>"Rifqi", "val1"=>"0004", "val2"=>"18/11/2008"),
-    array("val0"=>"Tya", "val1"=>"0005", "val2"=>"25/06/2008")
-);
